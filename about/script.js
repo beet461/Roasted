be00921d750e55465b1b43f330cfdbf67d4c12da
beet461 <rushvithanthagiri@gmail.com>
2021-06-20 13:25:04 +0000

    FINISHED YAYAYAYAYY. Set up disc server and thats it

---
+++ about/script.js
@@ -0,0 +1,6 @@
+ab_rst = `We here at Roasted!™  have created this website for all the young humans out there wanting to fight back. Whether someone is bullying you or making you feel sad, this is the place to get back at them. We provide our service 100% free of charge, you can get insults ranging from weak (for the little annoyances) all the way to EPIC (will make them cry themselves to sleep). Click the generate button on our homepage and get your comeback today!`
+
+function insert_text() {
+    document.getElementById('ab-rst').innerHTML = ab_rst
+    document.getElementById('credits').innerHTML = credits
+}
